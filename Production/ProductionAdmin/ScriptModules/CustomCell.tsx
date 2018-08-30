f3936eae5223652e51ad4dd46f54e9a35334f91e
DECIDEWARE\mperdeck <mperdeck@decideware.com>
2018-08-30 17:16:42 +1000

    Add CustomCell first version

---
+++ Production/ProductionAdmin/ScriptModules/CustomCell.tsx
@@ -0,0 +1,21 @@
+﻿// /// <reference path="kendo.all.d.ts" />
+
+declare var React: typeof import("react");
+declare var ReactDOM: typeof import("react-dom");
+
+declare var Grid: typeof import("kendo-ui");
+
+
+
+export interface CustomCellProps { message: string; }
+
+export class CustomCell extends GridCell {
+    render() {
+        return (
+            <td>
+                <input disabled type="checkbox" checked={this.props.dataItem[this.props.field]} />
+            </td>
+        );
+    }
+}
+
