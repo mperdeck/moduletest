08744c71471680ba050f02ca80bcaa14b511a565
DECIDEWARE\mperdeck <mperdeck@decideware.com>
2018-08-31 11:15:42 +1000

    attempt 2 at GridCell

--- Production/ProductionAdmin/ScriptModules/CustomCell.tsx
+++ Production/ProductionAdmin/ScriptModules/CustomCell.tsx
@@ -1,15 +1,27 @@
 ﻿// /// <reference path="kendo.all.d.ts" />
 
+
+// This definition based on
+// https://www.telerik.com/kendo-react-ui/components/grid/
+
+
 declare var React: typeof import("react");
 declare var ReactDOM: typeof import("react-dom");
 
-declare var Grid: typeof import("kendo-ui");
+//declare var Grid: typeof import("kendo-ui");
 
+// Kendo uses different namespaces in its index.d.ts file!
+// If you need a type, make sure you get the right namespace.
+// C:\Decideware\trials\moduletest\node_modules\@types\kendo-ui\index.d.ts
 
+// Note that the index.d.ts file gets loaded automatically by virtue of sitting in
+// node_modules/@types
+
+declare var GridCell: kendo.ui.GridColumn;
 
 export interface CustomCellProps { message: string; }
 
-export class CustomCell extends GridCell {
+export class CustomCell implements kendo.ui.GridColumn {
     render() {
         return (
             <td>
