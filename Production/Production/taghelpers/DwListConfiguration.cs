d612ef339dba879ceb47606c0d1a37e1d051b6c7
mperdeck@gmail.com <mperdeck@gmail.com>
2018-09-22 19:25:24 +1000

    th - childless

---
+++ Production/Production/taghelpers/DwListConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Production.taghelpers
+{
+    public class DwListConfiguration
+    {
+        public string ListName { get; set; }
+        public int ListId { get; set; }
+    }
+}
---
+++ Production/Production/taghelpers/DwListTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Production.taghelpers
+{
+    public class DwListTagHelper: TagHelper
+    {
+        public string ListName { get; set; }
+        public int ListId { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var dwListConfiguration = new DwListConfiguration()
+            {
+                ListName = ListName,
+                ListId = ListId
+            };
+
+            var dwListConfigurationSerialised = JsonConvert.SerializeObject(dwListConfiguration);
+
+            output.TagName = "script";
+            output.Content.SetHtmlContent(dwListConfigurationSerialised);
+        }
+    }
+}
